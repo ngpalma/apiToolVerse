3f780f3c863fe1c6b143f6d0cbb8a1ff6596d0cf
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-24 19:51:35 -0300

    origin

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ var corsOptions = {
    origin: false 
 }
 
+server.use(cors(corsOptions))
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(morgan('dev'));
 server.use(cookieParser())
-server.use(cors(corsOptions))
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", {origin: "*"}); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
