5fd1aa835bb0ce01ea7b18bd0cb5cbd9b39a1aa6
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-24 19:44:11 -0300

    corsOptions

--- src/app.js
+++ src/app.js
@@ -8,13 +8,16 @@ const server = express();
 const mercadopago = require("mercadopago")
 server.name = 'API';
 const cors = require('cors')
-
+var corsOptions = {
+  origin: 'https://clienttoolverse-production.up.railway.app/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(morgan('dev'));
 server.use(cookieParser())
-server.use(cors())
+server.use(cors(corsOptions))
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
