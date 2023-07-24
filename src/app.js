9b92733ac389aa9d4e19b00098fa98fc08d11594
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-24 20:55:09 -0300

    ruta

--- src/app.js
+++ src/app.js
@@ -7,29 +7,20 @@ const cookieParser =require('cookie-parser');
 const server = express();
 const mercadopago = require("mercadopago")
 server.name = 'API';
-const cors = require('cors')
-// var corsOptions = {
-//    origin: false 
-// }
 
 
-
-
-
-server.use(cors());
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(morgan('dev'));
 server.use(cookieParser())
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://clienttoolverse-production.up.railway.app/'); // Reemplaza con el dominio correcto del front-end
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 server.use(routes);
 
 
