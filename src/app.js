dafef80734054b96d4cf798550a8575c76596057
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-24 13:50:53 -0300

    cors again

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ server.use(morgan('dev'));
 server.use(cookieParser())
 server.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Origin', "https://clienttoolverse-production.up.railway.app/");
+  res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
