0ed12bceda60e89326c75cc0139f3c497aff5b9d
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-24 13:21:46 -0300

    cors localhost

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(morgan('dev'));
 server.use(cookieParser())
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Reemplaza con el dominio correcto del front-end
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Reemplaza con el dominio correcto del front-end
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
